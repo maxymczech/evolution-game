b461672badf765fc0ef0e0b2d7ef2ba165831150
Maksym Shcherban <shchema1@fel.cvut.cz>
2020-08-15 20:36:11 +0200

    update webpack/babel/eslint configuration

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,18 @@ module.exports = {
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
+          presets: [
+            '@babel/preset-env'
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties"
+          ]
         }
       }, {
-        loader: 'eslint-loader'
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true
+        }
       }]
     }, {
       test: /\.s[ac]ss$/i,
